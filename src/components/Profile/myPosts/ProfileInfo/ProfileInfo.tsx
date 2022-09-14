517a785b067b809c15f8f720a3dfef294f950f35
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-09-14 14:44:52 +0200

    85, profile status on FC and hookes

--- src/components/Profile/myPosts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/myPosts/ProfileInfo/ProfileInfo.tsx
@@ -3,6 +3,7 @@ import s from './ProfileInfo.module.css'
 import Preloader from "../../../Common/Preloader/Preloader";
 import {UserProfileType} from "../../../../Redux/reducers/ProfilePageReducer";
 import ProfileStatus from "./ProfileStatus";
+import {ProfileStatusHooks} from "./ProfileStatusHooks";
 //
 // const BackgroundCover = require('./BGcovering.jpg');
 
@@ -22,16 +23,19 @@ export const ProfileInfo = (props: ProfileInfoType) => {
                 {/*<img src={BackgroundCover} alt='background cover'/>*/}
             </div>
             <div className={s.descriptionBlock}>
-               <img src={props.profile.photos.large}/>
+                <img src={props.profile.photos.large}/>
             </div>
             <div>
-                <ProfileStatus status={props.status}
-                               updateStatus={props.updateStatus}/>
+                {/*<ProfileStatus status={props.status}*/}
+                {/*               updateStatus={props.updateStatus}/>*/}
+                <ProfileStatusHooks status={props.status}
+                                    updateStatus={props.updateStatus}/>
             </div>
             <div>
                 <div>{`${props.profile.fullName}`}</div>
                 <div>About me: {props.profile.aboutMe && `${props.profile.aboutMe}`} </div>
-                <div>Looking for a job: {props.profile.lookingForAJobDescription && `${props.profile.lookingForAJobDescription}`}</div>
+                <div>Looking for a
+                    job: {props.profile.lookingForAJobDescription && `${props.profile.lookingForAJobDescription}`}</div>
                 <div>Contact me: {props.profile.contacts && `${Object.keys(props.profile.contacts)}`}</div>
             </div>
         </div>
